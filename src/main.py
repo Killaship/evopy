43ffc5c0aeae34be85e24fd7ce751f5cc6076540
Killaship <69988679+Killaship@users.noreply.github.com>
2021-10-13 12:51:52 +0000

    wqeart

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("placeholder")
+pop = int(input("Population?"))
+ticks = int(input("Ticks?"))
+food = int(input("Food?"))
+genome = []
+# gene 1 decides max life
+# gene 2 decides death probability
+# gene 3 decides reproduction chance
+# gene 4 decides 
+
+
+def tick():
+  if(ticks != 0):
+    for i in range(pop):
+
+
+    
+
